f0d4bb398ea5ec76db918d86d15ab21abbcfe507
masindes <maasinde.sylvester@gmail.com>
2024-10-02 17:50:43 +0300

    update inde.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, str) {
+    return array.filter((newstr) => newstr.toLowerCase() === str.toLowerCase());
+  }
+  
+  function fuzzyMatch(array, string) {
+    return array.filter((letter) => letter.slice(0, string.length) === string);
+  }
+  
+  function matchName(array, string) {
+    return array.filter((element) => element.name === string);
+  }
